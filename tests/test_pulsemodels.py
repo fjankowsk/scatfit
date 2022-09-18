b64e93e71a2a88f897be1ffd8757f87fce908280
Fabian Jankowski <fjankowsk@gmail.com>
2022-09-18 19:22:12 +0200

    Rename previous test. Add test to ensure that the pulse energy, i.e. fluence, is conserved during scattering.

--- tests/test_pulsemodels.py
+++ tests/test_pulsemodels.py
@@ -3,10 +3,10 @@ import numpy as np
 import scatfit.pulsemodels as pulsemodels
 
 
-def test_fluence_conservation_gaussian():
+def test_normalisation_gaussian():
     """
     Check that the Gaussian is correctly normalised, i.e.
-    have the correct fluence.
+    has the correct fluence.
     """
 
     plot_range = np.linspace(-200.0, 200.0, num=2000)
@@ -21,6 +21,31 @@ def test_fluence_conservation_gaussian():
         )
 
         assert np.allclose(res, fluence)
+
+
+def test_conservation_of_fluence():
+    """
+    Check that the energy, i.e. fluence, is conserved during
+    scattering.
+    """
+
+    plot_range = np.linspace(-1000.0, 1000.0, num=8000)
+
+    center = 10.0
+    sigma = 2.5
+    dc = 0.0
+
+    models = [pulsemodels.scattered_profile, pulsemodels.scattered_gaussian_pulse]
+
+    for model in models:
+        for fluence in [0.1, 1.0, 5.0, 10.0, 50.0, 100.0, 500.0, 1000.0]:
+            for taus in [5.0, 10.0, 15.0, 20.0, 30.0, 50.0]:
+                res = np.trapz(
+                    model(plot_range, fluence, center, sigma, taus, dc),
+                    x=plot_range,
+                )
+
+                assert np.allclose(res, fluence)
 
 
 if __name__ == "__main__":
