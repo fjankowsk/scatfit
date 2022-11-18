612162efa83454127eabe0a151aac7a35b052283
Fabian Jankowski <fjankowsk@gmail.com>
2022-11-18 17:41:45 +0000

    Increase the coverage of the comparison test.

--- tests/test_pulsemodels.py
+++ tests/test_pulsemodels.py
@@ -55,30 +55,34 @@ def test_agreement_analytical_and_full_convolution_model():
 
     plot_range = np.linspace(-1000.0, 1000.0, num=8000)
 
-    center = 10.0
-    sigma = 2.5
     dc = 0.0
 
     full_model = pulsemodels.scattered_profile
     analytical_model = pulsemodels.scattered_gaussian_pulse
 
-    for fluence in [0.1, 1.0, 5.0, 10.0, 50.0, 100.0, 500.0, 1000.0]:
-        for taus in [5.0, 10.0, 15.0, 20.0, 30.0, 50.0]:
-            curve_full = full_model(plot_range, fluence, center, sigma, taus, dc)
-            curve_analytical = analytical_model(
-                plot_range, fluence, center, sigma, taus, dc
-            )
+    for fluence in np.geomspace(0.1, 1000.0, num=10):
+        for center in np.linspace(-100.0, 100.0, num=10):
+            for sigma in np.geomspace(2.0, 50.0, num=10):
+                for taus in np.geomspace(1.0, 50.0, num=10):
+                    curve_full = full_model(
+                        plot_range, fluence, center, sigma, taus, dc
+                    )
+                    curve_analytical = analytical_model(
+                        plot_range, fluence, center, sigma, taus, dc
+                    )
 
-            # ensure that the energy, i.e. fluence, is the same
-            fluence_full = np.trapz(curve_full, x=plot_range)
-            fluence_analytical = np.trapz(curve_analytical, x=plot_range)
+                    # ensure that the energy, i.e. fluence, is the same
+                    fluence_full = np.trapz(curve_full, x=plot_range)
+                    fluence_analytical = np.trapz(curve_analytical, x=plot_range)
 
-            assert np.allclose(fluence_full, fluence_analytical)
+                    rel_error = np.abs(fluence_analytical - fluence_full) / fluence_full
+                    # print(fluence_full, fluence_analytical, rel_error)
+                    assert rel_error < 0.01
 
-            # ensure that curves differ little
-            rel_residual = (curve_analytical - curve_full) / np.max(curve_full)
-            assert np.max(rel_residual) < 0.05
-            assert np.median(rel_residual) < 0.01
+                    # ensure that curves differ little
+                    residual = curve_analytical - curve_full
+                    # print(np.max(residual))
+                    assert np.allclose(curve_analytical, curve_full)
 
 
 if __name__ == "__main__":
