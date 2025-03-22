dc6cf2f0414ec7531f7f60156ebb5052f3aac9b2
Fabian Jankowski <fjankowsk@gmail.com>
2025-03-22 15:05:20 +0100

    Add regression tests to check invalid input parameters.

--- tests/test_pulsemodels.py
+++ tests/test_pulsemodels.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import scatfit.pulsemodels as pulsemodels
 
@@ -85,6 +86,136 @@ def test_agreement_analytical_and_full_convolution_model():
                     assert np.median(rel_residual) < 1e-8
                     assert np.mean(rel_residual) < 0.01
                     assert np.all(rel_residual < 0.07)
+
+
+def test_invalid_parameters():
+    """
+    Check the handling of invalid input parameters.
+    """
+
+    plot_range = np.linspace(-1000.0, 1000.0, num=8000)
+
+    fluence_valid = 10.0
+    center = 0.0
+    sigma_valid = 2.5
+    taus_valid = 5.0
+    dc = 0.0
+    f_lo_valid = 55.0
+    f_hi_valid = 65.0
+    nfreq_valid = 3
+
+    # normed gaussian
+    model = pulsemodels.gaussian_normed
+
+    for fluence in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(plot_range, fluence, center, sigma_valid)
+
+    for sigma in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(plot_range, fluence_valid, center, sigma)
+
+    # scattered gaussian pulse
+    model = pulsemodels.scattered_gaussian_pulse
+
+    for fluence in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(plot_range, fluence, center, sigma_valid, taus_valid, dc)
+
+    for sigma in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(plot_range, fluence_valid, center, sigma, taus_valid, dc)
+
+    for taus in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(plot_range, fluence_valid, center, sigma_valid, taus, dc)
+
+    # bandintegrated model
+    model = pulsemodels.bandintegrated_model
+
+    for fluence in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence,
+                center,
+                sigma_valid,
+                taus_valid,
+                dc,
+                f_lo_valid,
+                f_hi_valid,
+                nfreq_valid,
+            )
+
+    for sigma in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence_valid,
+                center,
+                sigma,
+                taus_valid,
+                dc,
+                f_lo_valid,
+                f_hi_valid,
+                nfreq_valid,
+            )
+
+    for taus in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence_valid,
+                center,
+                sigma_valid,
+                taus,
+                dc,
+                f_lo_valid,
+                f_hi_valid,
+                nfreq_valid,
+            )
+
+    for f_lo in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence_valid,
+                center,
+                sigma_valid,
+                taus_valid,
+                dc,
+                f_lo,
+                f_hi_valid,
+                nfreq_valid,
+            )
+
+    for f_hi in [0.0, -1e-5, -1.0, -10.0]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence_valid,
+                center,
+                sigma_valid,
+                taus_valid,
+                dc,
+                f_lo_valid,
+                f_hi,
+                nfreq_valid,
+            )
+
+    for nfreq in [1, 0, -1e-5, -1, -2, -10]:
+        with pytest.raises(AssertionError):
+            model(
+                plot_range,
+                fluence_valid,
+                center,
+                sigma_valid,
+                taus_valid,
+                dc,
+                f_lo_valid,
+                f_hi_valid,
+                nfreq,
+            )
 
 
 if __name__ == "__main__":
