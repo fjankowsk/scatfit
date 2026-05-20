360f8fac650e7415bbcf209a54ae17f8ee795d5f
Fabian Jankowski <fjankowsk@gmail.com>
2026-05-20 19:00:42 +0200

    Remove the maximum value for now.

--- scatfit/apps/fit_frb.py
+++ scatfit/apps/fit_frb.py
@@ -559,9 +559,7 @@ def fit_profile_model(fit_range, profile, smodel, params):
         )
 
         # set the minimum to the sampling time of the data
-        model.set_param_hint(
-            f"{prefix}sigma", value=1.5, min=params["tsamp"], max=100.0
-        )
+        model.set_param_hint(f"{prefix}sigma", value=1.5, min=params["tsamp"])
 
         arg_list = list(inspect.signature(scat_model).parameters.keys())
 
