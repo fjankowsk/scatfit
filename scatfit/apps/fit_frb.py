f7a5e657e9ba2c475a2b4235bca78b5ab432309d
Fabian Jankowski <fjankowsk@gmail.com>
2026-05-20 18:49:38 +0200

    Bump up the sigma maximum.

--- scatfit/apps/fit_frb.py
+++ scatfit/apps/fit_frb.py
@@ -559,7 +559,9 @@ def fit_profile_model(fit_range, profile, smodel, params):
         )
 
         # set the minimum to the sampling time of the data
-        model.set_param_hint(f"{prefix}sigma", value=1.5, min=params["tsamp"], max=50.0)
+        model.set_param_hint(
+            f"{prefix}sigma", value=1.5, min=params["tsamp"], max=100.0
+        )
 
         arg_list = list(inspect.signature(scat_model).parameters.keys())
 
